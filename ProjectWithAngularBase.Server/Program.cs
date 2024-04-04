ef02ca89ab9f7f7cbeb7b98a06c3b7d16a78e824
Joao Marques <joao.marques@gclaims.com.br>
2024-04-04 19:23:14 -0300

    Adicionando sqlServer

--- ProjectWithAngularBase.Server/Program.cs
+++ ProjectWithAngularBase.Server/Program.cs
@@ -1,9 +1,9 @@
+using App.Providers;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,12 +12,16 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+builder.Services.AddDbContext<EntityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 app.UseHttpsRedirection();
 
