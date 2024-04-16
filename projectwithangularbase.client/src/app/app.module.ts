058aefcecb3911e2c0fa7a87cb449faa0a6db359
Joao Marques <joao.marques@gclaims.com.br>
2024-04-16 12:53:57 -0300

    Criação do Log  e componente para log

--- projectwithangularbase.client/src/app/app.module.ts
+++ projectwithangularbase.client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
