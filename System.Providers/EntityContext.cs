7db3340a143b630e5b7c1586f7fdbdc189c0365e
Joao Marques <joao.marques@gclaims.com.br>
2024-04-16 13:09:40 -0300

    Ajuste de erros ao  trocar nome das pastas

--- System.Providers/EntityContext.cs
+++ System.Providers/EntityContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System.Entities.Logs;
+using System.Entities;
 using System.Providers;
 
 
--- System.Providers/Providers/LogProvider.cs
+++ System.Providers/Providers/LogProvider.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using System.Entities.Logs;
+using System.Entities;
 
 namespace System.Providers
 {
